94643e9c54d38a3b4dd41412d5e7f346f4ea45f0
= <henriquediogo434@gmail.com>
2024-06-10 16:52:54 -0300

    Meu Segundo COmponent

--- primeiro-projeto/src/app/app.component.ts
+++ primeiro-projeto/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {MeuPrimeiroComponent2} from '/meu-primeiro2/MeuPrimeiroComponent2';
+
 @Component({
   selector: 'app-root',
   standalone: true,
