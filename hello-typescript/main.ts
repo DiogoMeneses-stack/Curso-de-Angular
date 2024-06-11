77c3eeba7aa3eafde4845a9cc8a74ba336117d12
= <henriquediogo434@gmail.com>
2024-06-11 10:33:42 -0300

    Typescrit

---
+++ hello-typescript/main.ts
@@ -0,0 +1,23 @@
+var minhaVariavel = 'minhaVariavel';
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+// Es 2015
+let num = 10;
+const pi = 3.14;
+
+var numeros = [1, 2, 3];
+
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+//Typescipt
+numeros.map(valor => {return valor * 2;})
+
+class Matematica {soma(x, y){return x + y;}}
+
+var n1: string = 'sdfsgdfg';
+var n1: any = 'sdfsgdfg'; // any trsansforma em qualquer valor
